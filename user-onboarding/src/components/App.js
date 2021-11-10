4dd784363c29a0f5f8dd8afe0146b92096bf16c6
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-11-10 14:15:14 -0600

    Preliminary file setup

---
+++ user-onboarding/src/components/App.js
@@ -0,0 +1,13 @@
+import Form from './Form';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="">
+        <h1>Add a New User</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
